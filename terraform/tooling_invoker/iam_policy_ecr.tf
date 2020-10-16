37d13ca27cc9cd14012216deb87f805c28a1de87
Erik Schierboom <erik_schierboom@hotmail.com>
2020-10-16 17:14:30 +0200

    Add C# test runner

--- terraform/tooling_invoker/iam_policy_ecr.tf
+++ terraform/tooling_invoker/iam_policy_ecr.tf
@@ -21,7 +21,8 @@ resource "aws_iam_policy" "ecr" {
                 "ecr:DescribeImageScanFindings"
             ],
             "Resource": [
-              "arn:aws:ecr:${var.region}:${local.aws_account_id}:repository/ruby-test-runner"
+              "arn:aws:ecr:${var.region}:${local.aws_account_id}:repository/ruby-test-runner",
+              "arn:aws:ecr:${var.region}:${local.aws_account_id}:repository/csharp-test-runner"
             ]
         }, {
             "Effect": "Allow",
