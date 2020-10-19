f673c01179dd7d08bbbab680cf328b73c52b8d08
Jeremy Walker <jez.walker@gmail.com>
2020-10-19 15:23:25 +0100

    Update AMI

--- terraform/tooling_invoker/ami.sh
+++ terraform/tooling_invoker/ami.sh
@@ -1,21 +1,13 @@
+#####################
+# Run setup as root #
+#####################
+sudo su -
+
 ###################
 # System packages #
 ###################
-sudo apt-get -y update
-sudo apt-get install -y wget git make unzip uidmap
-
-################
-# Add the user #
-################
-sudo su -
-groupadd exercism
-
-# TODO: Can we avoid giving this sudo
-useradd -G sudo -m -s /bin/bash worker
-echo "worker    ALL=NOPASSWD: ALL" >/etc/sudoers.d/worker
-chmod 440 /etc/sudoers.d/worker
-
-sudo su -l worker
+apt-get -y update
+apt-get install -y wget git make unzip uidmap
 
 #################
 # Initial Setup #
@@ -29,14 +21,14 @@ pushd ~/install/
 wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
 tar -xzvf ruby-install-0.7.0.tar.gz
 pushd ruby-install-0.7.0/
-  sudo make install
+  make install
 popd
 RUBY_CONFIGURE_OPTS=--disable-install-doc ruby-install ruby 2.6.6
 
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 pushd chruby-0.3.9/
-  sudo make install
+  make install
 popd
 sed -i '1s/^/source \/usr\/local\/share\/chruby\/chruby.sh\nchruby 2.6.6\n/' ~/.profile
 source /usr/local/share/chruby/chruby.sh
@@ -49,46 +41,119 @@ gem install bundler:2.1.4
 ###############
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
+
+#######################
+# Add the worker user #
+#######################
+groupadd exercism
+
+useradd -m -s /bin/bash exercism_worker
+echo "exercism_worker    ALL=NOPASSWD: ALL" >/etc/sudoers.d/exercism_worker
+su exercism_worker
+  sed -i '1s/^/source \/usr\/local\/share\/chruby\/chruby.sh\nchruby 2.6.6\n/' ~/.profile
+  sed -i '1s/^/PATH=\/opt\/container_tools:$PATH\n/' ~/.profile
+  source /usr/local/share/chruby/chruby.sh
+  chruby ruby-2.6.6
+  gem install bundler:2.1.4
+exit
+
+########################
+# Add the manager user #
+########################
+useradd -m -s /bin/bash exercism_manager
+echo "exercism_manager    ALL=NOPASSWD: ALL" >/etc/sudoers.d/exercism_manager
+su exercism_manager
+  sed -i '1s/^/source \/usr\/local\/share\/chruby\/chruby.sh\nchruby 2.6.6\n/' ~/.profile
+  sed -i '1s/^/PATH=\/opt\/container_tools:$PATH\n/' ~/.profile
+  source /usr/local/share/chruby/chruby.sh
+  chruby ruby-2.6.6
+  gem install bundler:2.1.4
+exit
 
 #############################
 # Setup container tools dir #
 #############################
-sudo mkdir /opt/container_tools
-sudo chown worker:exercism /opt/container_tools
-
-sed -i '1s/^/PATH=\/opt\/container_tools:$PATH\n/' ~/.profile
-source ~/.profile
+DIR = "/opt/container_tools"
+mkdir $DIR
+chown exercism_manager:exercism $DIR
+chmod 770 $DIR
 
 ########################
 # Install worker tools #
 ########################
-
-# TODO: Check the tools are owned by the correct 
-# user/group and think about permissions
-wget https://exercism-ops.s3-eu-west-1.amazonaws.com/binaries/runc -O  /opt/container_tools/runc
-chmod 755 /opt/container_tools/runc
+RUNC="/opt/container_tools/runc"
+wget https://exercism-ops.s3-eu-west-1.amazonaws.com/binaries/runc -O $RUNC
+chown exercism_worker:exercism $RUNC
+chmod 500 $RUNC
+unset RUNC
 
 #####################
 # Install img tools #
 #####################
-
-# TODO: Check the tools are owned by the correct 
-# user/group and think about permissions
-wget https://exercism-ops.s3-eu-west-1.amazonaws.com/binaries/img -O  /opt/container_tools/img
-chmod 755 /opt/container_tools/img
+FILE="/opt/container_tools/img"
+wget https://exercism-ops.s3-eu-west-1.amazonaws.com/binaries/img -O $IMG
+chown exercism_manager:exercism $IMG
+chmod 500 $IMG
+unset IMG
 
 ########################
 # Setup Jobs Directory #
 ########################
-sudo mkdir /opt/jobs/
-sudo chown worker:exercism /opt/jobs/
+DIR="/opt/jobs"
+mkdir $DIR
+chown exercism_worker:exercism $DIR
+chmod 700 $DIR
+unset DIR
 
 ##############################
-# Setup containers Directory #
+# Setup containers directory #
 ##############################
-sudo mkdir /opt/containers/
-sudo chown worker:exercism /opt/containers/
+DIR="/opt/containers"
+mkdir $DIR
+chown exercism_manager:exercism $DIR
+chmod 740 $DIR
+unset DIR
+
+###########################
+# Install tooling invoker #
+###########################
+DIR="/opt/tooling-invoker"
+mkdir $DIR
+chown exercism_worker:exercism $DIR
+chmod 700 $DIR
+
+# TODO: Pull this from AWS'Git thing
+cd $DIR/..
+git clone https://github.com/exercism/tooling-invoker.git
+pushd tooling-invoker
+  bundle install
+popd
+unset DIR
+
+
+###########################
+# Install tooling manager #
+###########################
+
+DIR="/opt/tooling-manager"
+mkdir $DIR
+chown exercism_manager:exercism $DIR
+chmod 700 $DIR
+
+# TODO: Pull this from AWS's Git thing
+cd $DIR/..
+git clone https://github.com/exercism/tooling-manager.git
+pushd tooling-manager
+  bundle install
+popd
+unset DIR
+
+
+
+
+
+
 
 ########################################
 # TEMPORARY: Download Ruby Test Runner #
@@ -111,72 +176,14 @@ popd
 
 ln -s $CONTAINER_DIR /opt/containers/ruby-test-runner/current
 
-########################################
-# TEMPORARY: Download C# Test Runner #
-########################################
-
-CONTAINER_DIR="/opt/containers/csharp-test-runner/releases/latest"
-echo $CONTAINER_DIR
-mkdir -p $CONTAINER_DIR
-
-pushd $CONTAINER_DIR
-  /opt/container_tools/img logout
-
-  aws ecr get-login-password --region eu-west-2 | /opt/container_tools/img login -u AWS --password-stdin 591712695352.dkr.ecr.eu-west-2.amazonaws.com
-
-  /opt/container_tools/img pull -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/csharp-test-runner:latest
-  /opt/container_tools/img unpack -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/csharp-test-runner:latest
-  chmod -R a-w rootfs
-  chmod -R go-rwx rootfs
-popd
-
-ln -s $CONTAINER_DIR /opt/containers/csharp-test-runner/current
-
-##########################################
-# TEMPORARY: Download Elixir Test Runner #
-##########################################
-
-CONTAINER_DIR="/opt/containers/elixir-test-runner/releases/latest"
-echo $CONTAINER_DIR
-mkdir -p $CONTAINER_DIR
-
-pushd $CONTAINER_DIR
-  /opt/container_tools/img logout
-
-  aws ecr get-login-password --region eu-west-2 | /opt/container_tools/img login -u AWS --password-stdin 591712695352.dkr.ecr.eu-west-2.amazonaws.com
-
-  /opt/container_tools/img pull -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/elixir-test-runner:latest
-  /opt/container_tools/img unpack -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/elixir-test-runner:latest
-  chmod -R a-w rootfs
-  chmod -R go-rwx rootfs
-popd
-
-ln -s $CONTAINER_DIR /opt/containers/elixir-test-runner/current
-
-##############################################
-# TEMPORARY: Download JavaScript Test Runner #
-##############################################
-
-CONTAINER_DIR="/opt/containers/javascript-test-runner/releases/latest"
-echo $CONTAINER_DIR
-mkdir -p $CONTAINER_DIR
-
-pushd $CONTAINER_DIR
-  /opt/container_tools/img logout
-
-  aws ecr get-login-password --region eu-west-2 | /opt/container_tools/img login -u AWS --password-stdin 591712695352.dkr.ecr.eu-west-2.amazonaws.com
-
-  /opt/container_tools/img pull -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/javascript-test-runner:latest
-  /opt/container_tools/img unpack -state /tmp/state-img 591712695352.dkr.ecr.eu-west-2.amazonaws.com/javascript-test-runner:latest
-  chmod -R a-w rootfs
-  chmod -R go-rwx rootfs
-popd
-
-ln -s $CONTAINER_DIR /opt/containers/javascript-test-runner/current
-
-
 #############################
 # TEMPORARY: Run the worker #
 #############################
 cd /opt/tooling-invoker
-EXERCISM_ENV=production bundle exec bin/worker 
+git fetch && git reset --hard origin/main && EXERCISM_ENV=production bundle exec bin/worker
+
+#############################
+# TEMPORARY: Run the manager #
+#############################
+cd /opt/tooling-manager
+git fetch && git reset --hard origin/main && EXERCISM_ENV=production bundle exec bin/manager
