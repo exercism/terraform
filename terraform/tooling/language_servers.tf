a01bc5c6ef241046c3c56bdf7cbdb8faf5d4bba0
Karlo Soriano <karlo@thalamus.ai>
2020-11-20 21:13:12 +0800

    Add language_servers.tf

---
+++ terraform/tooling/language_servers.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "language_servers" {
+  for_each = var.ecr_language_servers
+  name     = each.key
+
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration { scan_on_push = false }
+}
