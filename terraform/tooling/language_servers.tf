b9f781c14259886ba518b6a7ce8adfc70a496a32
Karlo Soriano <karlo@thalamus.ai>
2020-11-20 21:21:57 +0800

    Fix variable name

--- terraform/tooling/language_servers.tf
+++ terraform/tooling/language_servers.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "language_servers" {
-  for_each = var.ecr_language_servers
+  for_each = var.ecr_language_server_repos
   name     = each.key
 
   image_tag_mutability = "MUTABLE"
