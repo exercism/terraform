263f7e25d230da0aa070e5b8ee0ab39606aa3e79
Karlo Soriano <karlo@thalamus.ai>
2020-11-20 20:43:53 +0800

    Add language servers

---
+++ terraform/language_servers/main.tf
@@ -0,0 +1,26 @@
+variable "region" {}
+
+variable "aws_account_id" {}
+variable "aws_iam_policy_document_assume_role_ecs" {}
+variable "aws_iam_policy_read_dynamodb_config" {}
+variable "aws_iam_policy_write_to_cloudwatch" {}
+variable "aws_iam_role_ecs_task_execution" {}
+
+variable "aws_vpc_main" {}
+variable "aws_subnet_publics" {}
+
+variable "http_port" {}
+
+variable "container_cpu" {}
+variable "container_memory" {}
+variable "container_count" {}
+
+provider "aws" {
+  region  = var.region
+  version = "~> 2.64"
+}
+
+data "aws_caller_identity" "current" {}
+locals {
+  aws_account_id = data.aws_caller_identity.current.account_id
+}
