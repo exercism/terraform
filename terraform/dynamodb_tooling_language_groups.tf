641624db53f26ffd39a18f0f98785bc6e8bf31b1
Jeremy Walker <jez.walker@gmail.com>
2021-01-12 12:49:17 +0000

    Add nim to language groups

--- terraform/dynamodb_tooling_language_groups.tf
+++ terraform/dynamodb_tooling_language_groups.tf
@@ -29,7 +29,7 @@ resource "aws_dynamodb_table_item" "tooling_language_r_all" {
   item = <<ITEM
 {
   "group": {"S": "representers-all"},
-  "languages": {"S": "clojure,common-lisp,csharp,elixir,elm,fsharp,j,javascript,python,ruby,rust"}
+  "languages": {"S": "clojure,common-lisp,csharp,elixir,elm,fsharp,j,javascript,nim,python,ruby,rust"}
 }
 ITEM
 }
@@ -41,7 +41,7 @@ resource "aws_dynamodb_table_item" "tooling_language_a_all" {
   item = <<ITEM
 {
   "group": {"S": "analyzers-all"},
-  "languages": {"S": "clojure,common-lisp,csharp,elixir,erlang,go,java,javascript,python,ruby,rust,scala,typescript"}
+  "languages": {"S": "clojure,common-lisp,csharp,elixir,erlang,go,java,javascript,nim,python,ruby,rust,scala,typescript"}
 }
 ITEM
 }
