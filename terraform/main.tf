b9842e2138beec2d879f403ec05ae3526ea93ce0
Jeremy Walker <jez.walker@gmail.com>
2021-04-10 22:36:33 +0100

    Add typescript representer

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,6 +84,7 @@ locals {
     "swift-test-runner",
     "tcl-test-runner",
     "typescript-analyzer",
+    "typescript-representer",
     "typescript-test-runner",
     "vimscript-test-runner",
     "x86-64-assembly-test-runner"
