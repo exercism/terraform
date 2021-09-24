dc13cf76474723417060fdca2d6958d263fd66fb
Jeremy Walker <jez.walker@gmail.com>
2021-09-24 15:22:33 +0100

    Update to real values for autoscaling

--- terraform/tooling_invoker/autoscaling_group.tf
+++ terraform/tooling_invoker/autoscaling_group.tf
@@ -1,12 +1,12 @@
 resource "aws_autoscaling_group" "main" {
   name = "Tooling Invokers"
 
-  desired_capacity = 3
-  max_size         = 4
-  min_size         = 2
+  min_size         = 8
+  desired_capacity = 10
+  max_size         = 12
   max_instance_lifetime = 86400
 
-  default_cooldown   = 600
+  default_cooldown   = 900
   capacity_rebalance = false
 
   launch_template {
@@ -14,13 +14,20 @@ resource "aws_autoscaling_group" "main" {
     version = "$Latest"
   }
 
-  health_check_grace_period = 300
+  health_check_grace_period = 900
   health_check_type         = "EC2"
   metrics_granularity       = "1Minute"
   protect_from_scale_in     = false
   service_linked_role_arn   = "arn:aws:iam::681735686245:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
   termination_policies      = ["OldestInstance"]
   vpc_zone_identifier       = var.aws_subnet_publics.*.id
+
+
+  tag {
+    key                 = "Name"
+    value               = "Autoscaling Tooling Runner"
+    propagate_at_launch = true
+  }
 
   enabled_metrics = [
     "GroupAndWarmPoolDesiredCapacity",
