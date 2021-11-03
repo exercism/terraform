246683a66c5d948ab4a0d950b923b6214bf1bb45
Jeremy Walker <jez.walker@gmail.com>
2021-11-03 14:35:25 +0000

    Fix priority

--- terraform/snippet_extractor/alb_target.tf
+++ terraform/snippet_extractor/alb_target.tf
@@ -1,6 +1,6 @@
 resource "aws_alb_listener_rule" "main" {
   listener_arn = var.aws_alb_listener_internal.arn
-  priority = 100
+  priority = 101
   action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.main.id
