1066c33c575889e784bda90668e178c653645e0d
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-23 11:03:09 +0100

    Morw work

--- terraform/transcriber/ami.sh
+++ terraform/transcriber/ami.sh
@@ -2,18 +2,25 @@
 # System packages #
 ###################
 sudo apt update
-sudo apt install -y ffmpeg nvidia-cuda-toolkit nvidia-driver-510
+sudo apt install -y ffmpeg
 
-##########################
-# Install OpenAI whisper #
-##########################
+##################
+# Update profile #
+##################
+echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+###########################
+# Install python packages #
+###########################
 sudo su -
+  python3 -m pip install --upgrade pip  
   pip install git+https://github.com/openai/whisper.git  
   pip uninstall torch
   pip cache purge
-  pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-  pip3 install tensorflow=2.9.2
-
-  echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> ~/.bashrc
-  source ~/.bashrc
+  pip install tensort  
+  pip install torch torchvision torchaudio torchtext --extra-index-url https://download.pytorch.org/whl/cu116
+  pip install tensorflow==2.9.2
+  pip install pyannote.audio
+  pip install speechbrain
 exit
