d6bbf5dbe734da26a72ef632278704df69be434c
Jeremy Walker <jez.walker@gmail.com>
2023-07-19 14:41:57 +0100

    Remove unused permissions

--- terraform/image_generator/main.tf
+++ terraform/image_generator/main.tf
@@ -1,9 +1,6 @@
 variable "region" {}
 variable "aws_account_id" {}
 variable "aws_subnet_publics" {}
-variable "aws_efs_mount_target_submissions" {}
-variable "aws_efs_access_point_submissions" {}
-variable "aws_security_group_efs_submissions_access" {}
 variable "aws_alb_listener_internal" {}
 
 provider "aws" {
