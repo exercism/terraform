7dc1176dc86caf5647841653ba95a471e70025bf
Jeremy Walker <jez.walker@gmail.com>
2023-09-14 15:46:16 +0200

    Update to add reason

--- terraform/ses/lambda_functions/bounce_handler/index.js
+++ terraform/ses/lambda_functions/bounce_handler/index.js
@@ -1,30 +1,29 @@
 "use strict";
 
-async function unsubscribe(email) {
-  console.log("EMAIL: " + email)
+async function unsubscribe(email, reason) {
   await fetch("https://internal.exercism.org/spi/unsubscribe_user", {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({ email, reason }),
   });
 }
 
-async function unsubscribeRecipients(recipients) {
+async function unsubscribeRecipients(recipients, reason) {
   if (recipients === null || recipients === undefined) return;
 
   await Promise.all(
-    recipients.map((recipient) => unsubscribe(recipient.emailAddress))
+    recipients.map((recipient) => unsubscribe(recipient.emailAddress, reason))
   );
 }
 
 async function processRecord(record) {
   const message = JSON.parse(record.Sns.Message);
   if (message.eventType === "Bounce")
-    await unsubscribeRecipients(message.bounce.bouncedRecipients);
+    await unsubscribeRecipients(message.bounce.bouncedRecipients, "bounce");
   else if (message.eventType === "Complaint")
-    await unsubscribeRecipients(message.bounce.complainedRecipients);
+    await unsubscribeRecipients(message.bounce.complainedRecipients, "complaint");
 }
 
 exports.handler = async (event, context) => {
